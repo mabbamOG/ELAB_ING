873f0aa6cb347700cad5cd232fd5ff6a8bbf8ceb
@mabbamOG <mariobarbara@protonmail.ch>
2017-07-10 12:43:32 +0200

    added des pattern example

--- client/utilities.py
+++ client/utilities.py
@@ -5,7 +5,6 @@ import os
 
 scriptpath = os.path.dirname(os.path.realpath(__file__))
 def loadiconbutton(s, color):
-    #filepath = 'icons/'+color+str(s).strip()+'.svg'
     filepath = f'{scriptpath}/../icons/{color}/{str(s).strip()}.svg'
     pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(filepath, width=50, height=50)
     img = Gtk.Image.new_from_pixbuf(pixbuf)
@@ -19,4 +18,3 @@ def loadalbumimage(s, size):
     img = Gtk.Image.new_from_pixbuf(pixbuf)
     img.set_hexpand(False)
     return img
-
